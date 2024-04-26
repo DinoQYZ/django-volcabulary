254e4dd85bb0e00933fd4fceefea6b9b1745b72c
DESKTOP-VQJQD1J\user <dino608.gg@gmail.com>
2024-04-26 15:21:12 +0800

    first commit

---
+++ main/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Vocabulary(models.Model):
+    word = models.ForeignKey(User, on_delete=models.CASCADE, related_name="voc", null=False)
+
+    def __str__(self) -> str:
+        return self.word
+    
+class Translation(models.Model):
+    voc = models.ForeignKey(Vocabulary, on_delete=models.CASCADE)
+    wd_type = models.TextField(max_length=50)
+    trans = models.TextField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.wd_type + " " + self.trans
