9552ed39d5bd4ab027a48cc10e879d2feba0db5b
DESKTOP-VQJQD1J\user <dino608.gg@gmail.com>
2024-05-01 00:33:45 +0800

    update add function (not done)

--- main/forms.py
+++ main/forms.py
@@ -1,28 +1,20 @@
 from django import forms
-from .models import Vocabulary, Translation
+from .models import Vocabulary
 
 class VocabularyForm(forms.ModelForm):
     vocabulary = forms.CharField(label="Vocabulary", max_length=255, required=True)
+    translations = forms.CharField(label="Translations", widget=forms.Textarea, required=False)
 
     class Meta:
         model = Vocabulary
         fields = ['word']
-
-    translations = forms.CharField(widget=forms.HiddenInput(), required=False)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if self.instance.pk:
-            translations = "\n".join(
-                f"{translation.trans},{translation.wd_type}" for translation in self.instance.translation_set.all()
-            )
-            self.fields['translations'].initial = translations
 
     def save(self, commit=True):
         vocabulary = super().save(commit=commit)
         if commit:
             translations_data = self.cleaned_data.get('translations')
             if translations_data:
-                for translation_data in translations_data.split("\n"):
-                    trans, wd_type = translation_data.split(',')
-                    Translation.objects
+                for line in translations_data.split("\n"):
+                    trans, wd_type = line.split(',')
+                    vocabulary.translation_set.create(trans=trans.strip(), wd_type=wd_type.strip())
+        return vocabulary
